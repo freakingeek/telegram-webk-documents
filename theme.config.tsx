5dbdcd174039b07d34a8cdb8ec5c7007b1685add
Yasin Silavi <ys.silavi@gmail.com>
2024-01-06 11:22:09 +0330

    feat: add more pages!

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,31 @@
 import React from 'react'
+import { useRouter } from 'next/router'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
   logo: <span>Telegram WebK Docs</span>,
+  useNextSeoProps() {
+    const { asPath } = useRouter()
+
+    if (asPath !== '/') {
+      return {
+        titleTemplate: '%s – Telegram WebK Docs'
+      }
+    }
+  },
   project: {
     link: 'https://github.com/sttatusx/telegram-webk-documents',
   },
-  chat: {
-    link: 'https://discord.com',
-  },
+  // chat: {
+  //   link: 'https://discord.com',
+  // },
   docsRepositoryBase: 'https://github.com/sttatusx/telegram-webk-documents',
   footer: {
-    text: 'Telegram WebK Docs',
+    text: `© ${new Date().getFullYear()} Telegram WebK Docs`,
   },
+  toc: {
+    backToTop: true
+  }
 }
 
 export default config
