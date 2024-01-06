b1e31810d2929a3c9b7601d3c303154aaea11222
Yasin Silavi <ys.silavi@gmail.com>
2024-01-06 10:51:43 +0330

    feat: configure nextra

---
+++ theme.config.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>Telegram WebK Docs</span>,
+  project: {
+    link: 'https://github.com/sttatusx/telegram-webk-documents',
+  },
+  chat: {
+    link: 'https://discord.com',
+  },
+  docsRepositoryBase: 'https://github.com/sttatusx/telegram-webk-documents',
+  footer: {
+    text: 'Telegram WebK Docs',
+  },
+}
+
+export default config
